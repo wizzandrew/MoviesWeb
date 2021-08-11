9ff662f7f060510fbef3ed1777fd58eb8112e1f2
Andrei Varzar <varzarandrew1999@gmail.com>
2021-08-11 15:42:47 +0200

    Finalised frontend

---
+++ MoviesWeb/Context/ContextModels/UserRatingDb.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesWeb.Context.ContextModels
+{
+    public class UserRatingDb
+    {
+        public int User_id { get; set; }
+        public int Movie_id { get; set; }
+        public float Rating { get; set; }
+
+        public UserRatingDb() { }
+        public UserRatingDb(int userId, int movieId, float rating)
+        {
+            User_id = userId;
+            Movie_id = movieId;
+            Rating = rating;
+        }
+    }
+}
